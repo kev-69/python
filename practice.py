b26a3079c283ac21a221b0e2e1d580576d99a962
kev-69 <bismarkobuobi19@gmail.com>
2025-02-24 17:03:29 +0000

    beginning python

---
+++ practice.py
@@ -0,0 +1,6 @@
+import math
+radius = float(input("what is the radius of the circle: "))
+pi_symbol =  "\u03C0"
+area = math.pi * radius ** 2
+print(f"The area of the circle with radius {radius} is: {area}")
+print(f"The area is {radius ** 2}{pi_symbol}cm")
