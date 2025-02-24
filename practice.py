cff115e4628f83ea2419e277e9422b3995b3d4c8
kev-69 <bismarkobuobi19@gmail.com>
2025-02-24 17:54:00 +0000

    testing the new configs i just made

--- practice.py
+++ practice.py
@@ -3,4 +3,4 @@ radius = float(input("what is the radius of the circle: "))
 pi_symbol =  "\u03C0"
 area = math.pi * radius ** 2
 print(f"The area of the circle with radius {radius} is: {area}")
-print(f"The area is {radius ** 2}{pi_symbol}cm")
+# print(f"The area is {radius ** 2}{pi_symbol}cm")
