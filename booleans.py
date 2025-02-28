eb5ac273f506a2f65c4643a61855dc6d9710c947
kev-69 <bismarkobuobi19@gmail.com>
2025-02-28 16:22:14 +0000

    this file containsa short note and code about booleans and the none variable type

---
+++ booleans.py
@@ -0,0 +1,9 @@
+# Booleans
+# Booleans, just like many other languages are operators that allow you to convey true of false statements.
+# They are mostly used in control flows and logic
+truth = True
+lies = False
+both = None # This is also called the noe data type (NoneType). used to store variables we dont want to assign yet
+
+print(truth)
+print(type(lies)) # returns the class type of the variable stored in lies
